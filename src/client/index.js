4a1361dbc6fc654fa794d133844e901560795439
rbc33 <ricardobenthem@gmail.com>
2025-02-19 17:20:47 +0100

    fixed npm i

--- src/client/index.js
+++ src/client/index.js
@@ -14,4 +14,16 @@ if (form) {
 else {
     console.error('Form element not found');
 }
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('/service-worker.js')
+            .then(registration => {
+            console.log('SW registered: ', registration);
+        })
+            .catch(registrationError => {
+            console.log('SW registration failed: ', registrationError);
+        });
+    });
+}
 //# sourceMappingURL=index.js.map
